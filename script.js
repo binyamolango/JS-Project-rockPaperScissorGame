ee9510afb47335751da63e3a7b738f177d0cf4e5
binyam yohannes <olangobinyam@gmail.com>
2023-05-17 14:26:38 +0300

    Prompt valid user input and make the round loop

--- script.js
+++ script.js
@@ -4,42 +4,54 @@ function getComputerChoice(arr){
     return arr[Math.floor(Math.random() * arr.length)];
     // return randomly either "Rock", "Paper" or "Scissors"
 }
-
+let user = 0; let comp = 0;
 function game(num){
     // play 5 round game that keeps score and report a winner or loser at the end
-    let user = 0; let comp = 0;
     function playRound(playerSelection, computerSelection){
         // return a string that declares the winner of the round
         if(playerSelection === "rock" && computerSelection === "scissor"){
             user++;
-            console.log("You : " + user, " Computer : " + comp);
         }
         else if(playerSelection === "rock" && computerSelection === "paper"){
             comp++;
-            console.log("You : " + user, " Computer : " + comp);
         }
         else if(playerSelection === "paper" && computerSelection === "rock"){
             user++;
-            console.log("You : " + user, " Computer : " + comp);
         }
         else if(playerSelection === "paper" && computerSelection === "scissor"){
             comp++;
-            console.log("You : " + user, " Computer : " + comp);
         }
         else if(playerSelection === "scissor" && computerSelection === "paper"){
             user++;
-            console.log("You : " + user, " Computer : " + comp);
         }
         else if(playerSelection === "scissor" && computerSelection === "rock"){
             comp++;
-            console.log("You : " + user, " Computer : " + comp);
         }
     }
     let userInput;
-    for(let i = 1; i <= num; i++){
+    let i = 1;
+    while(i <= num) {
         userInput = prompt("please write your selection from rock, paper and scissor").toLowerCase();
-        console.log(userInput);
-        playRound(userInput, getComputerChoice(["rock", "paper", "scissor"]));
+        let validInput = /^(rock|paper|scissor)$/i;
+        if(validInput.test(userInput)){
+            console.log(userInput);
+            playRound(userInput, getComputerChoice(["rock", "paper", "scissor"]));
+            console.log("You : " + user, " Computer : " + comp);
+            i++;
+        }
+        else {
+            console.log("Check your input please");
+        }
+    }
+    if(user > comp){
+        return "You Won! Computer Lost!";
+    }
+    else if(user < comp){
+        return "Computer Won! You Lost!";
+    }
+    else if(user === comp){
+        return "Draw!";
     }
 }
-game(5);
+let output = game(5);
+console.log(output);
