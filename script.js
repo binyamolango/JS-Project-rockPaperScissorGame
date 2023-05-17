584780245867b47997e99b2be0990b09dd1e092b
binyam yohannes <olangobinyam@gmail.com>
2023-05-17 13:46:35 +0300

    Receive input from the user

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
  
 
-function getComputerChoice(){
-    const arr = ["rock", "paper", "scissor"];
+function getComputerChoice(arr){
     return arr[Math.floor(Math.random() * arr.length)];
     // return randomly either "Rock", "Paper" or "Scissors"
 }
 
+function game(num){
+    // play 5 round game that keeps score and report a winner or loser at the end
+    function playRound(playerSelection, computerSelection){
+        // return a string that declares the winner of the round
+    }
+    let userInput;
+    userInput = prompt("please write your selection from rock, paper and scissor").toLowerCase();
+    console.log(userInput);
+    playRound(userInput, getComputerChoice(["rock", "paper", "scissor"]));
+}
+game(5);
