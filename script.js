77952fc76a4fb1660e3dd3555017f05d4aab6d71
binyam yohannes <olangobinyam@gmail.com>
2023-05-23 12:12:21 +0300

    add a user interface for the game

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
- 
-
 function getComputerChoice(arr){
     return arr[Math.floor(Math.random() * arr.length)];
     // return randomly either "Rock", "Paper" or "Scissors"
 }
+
 let user = 0; let comp = 0;
-function game(num){
-    // play 5 round game that keeps score and report a winner or loser at the end
-    function playRound(playerSelection, computerSelection){
-        // return a string that declares the winner of the round
-        if(playerSelection === "rock" && computerSelection === "scissor"){
-            user++;
-        }
-        else if(playerSelection === "rock" && computerSelection === "paper"){
-            comp++;
-        }
-        else if(playerSelection === "paper" && computerSelection === "rock"){
-            user++;
-        }
-        else if(playerSelection === "paper" && computerSelection === "scissor"){
-            comp++;
-        }
-        else if(playerSelection === "scissor" && computerSelection === "paper"){
-            user++;
-        }
-        else if(playerSelection === "scissor" && computerSelection === "rock"){
-            comp++;
-        }
+function playRound(e){
+    let playerSelection = this.classList.value;
+    let computerSelection = getComputerChoice(["rock", "paper", "scissor"]);
+    console.log(playerSelection, computerSelection)
+    // return a string that declares the winner of the round
+    if(playerSelection === "rock" && computerSelection === "scissor"){
+        user++;
     }
-    let userInput;
-    let i = 1;
-    while(i <= num) {
-        userInput = prompt("please write your selection from rock, paper and scissor").toLowerCase();
-        let validInput = /^(rock|paper|scissor)$/i;
-        if(validInput.test(userInput)){
-            console.log(userInput);
-            playRound(userInput, getComputerChoice(["rock", "paper", "scissor"]));
-            console.log("You : " + user, " Computer : " + comp);
-            i++;
-        }
-        else {
-            console.log("Check your input please");
-        }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        comp++;
     }
-    if(user > comp){
-        return "You Won! Computer Lost!";
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        user++;
     }
-    else if(user < comp){
-        return "Computer Won! You Lost!";
+    else if(playerSelection === "paper" && computerSelection === "scissor"){
+        comp++;
     }
-    else if(user === comp){
-        return "Draw!";
+    else if(playerSelection === "scissor" && computerSelection === "paper"){
+        user++;
+    }
+    else if(playerSelection === "scissor" && computerSelection === "rock"){
+        comp++;
+    }
+    div.textContent = `You :  ${user},  Computer :  ${comp}`;
+    if(user === 5){
+        div.textContent = `You :  ${user},  Computer :  ${comp}  You Won!`;
+        user = 0;
+        comp = 0;
+        return;
+    }
+    else if(comp === 5){
+        div.textContent = `You :  ${user},  Computer :  ${comp}  Computer Won!`;
+        comp = 0;
+        user = 0;
+        return;
     }
 }
-let output = game(5);
-console.log(output);
+
+const userInput = document.querySelectorAll("button");
+const div = document.createElement("div");
+const container = document.querySelector(".container");
+
+
+container.appendChild(div);
+userInput.forEach(input => input.addEventListener("click", playRound));
+div.textContent = `You :  ${user},  Computer :  ${comp}`;
